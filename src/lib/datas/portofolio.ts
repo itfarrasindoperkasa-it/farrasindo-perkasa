b6bee63074686979bf5021d1b5e5b40e193805e5
raflinaufal <raflinaufalalief204@gmail.com>
2025-11-25 15:58:27 +0700

    fix gambar portfolio

--- src/lib/datas/portofolio.ts
+++ src/lib/datas/portofolio.ts
@@ -1,10 +1,10 @@
 // lib/datas/portfolio.ts
-import imgPortfolio1 from "@/asset/images/portfolio/kodam-jaya.jpg";
+import imgPortfolio6 from "@/asset/images/portfolio/kodam-jaya.jpg";
 import imgPortfolio2 from "@/asset/images/portfolio/mass-concrete-pt-lianhua-cianjur.jpg";
 import imgPortfolio3 from "@/asset/images/portfolio/menara-centraraya.jpg";
-import imgPortfolio4 from "@/asset/images/portfolio/pengecoran-lapangan-tennis-pesanggrahan.jpg";
-import imgPortfolio5 from "@/asset/images/portfolio/mass-concrete-pt-lianhua-cianjur.jpg";
-import imgPortfolio6 from "@/asset/images/portfolio/rm-pagi-sore-puncak.jpg";
+import imgPortfolio5 from "@/asset/images/portfolio/pengecoran-lapangan-tennis-pesanggrahan.jpg";
+import imgPortfolio4 from "@/asset/images/portfolio/pengecoran-seklah-cianjur.jpg";
+import imgPortfolio1 from "@/asset/images/portfolio/rm-pagi-sore-puncak.jpg";
 import imgPortfolio7 from "@/asset/images/portfolio/rs-hermina-depok.jpg";
 
 // semua jadi string URL
